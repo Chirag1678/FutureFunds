2d0618018f553a745f946db929175efdbe146a51
VipulArora_2003 <vipularora9930@gmail.com>
2025-04-28 15:43:47 +0530

    [Vipul] Add: Adding user model

--- src/main/java/com/cg/futurefunds/model/User.java
+++ src/main/java/com/cg/futurefunds/model/User.java
@@ -1,8 +1,6 @@
 package com.cg.futurefunds.model;
 
-
-
-
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -10,43 +8,45 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
-
-import java.security.PrivateKey;
-
-import org.apache.logging.log4j.message.AsynchronouslyFormattable;
-import org.apache.tomcat.util.security.PrivilegedGetTccl;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 @Data
 @Entity
-@Table(name="Users")
+@Table(name = "users")
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
 
-
     @Id
-    @GeneratedValue(strategy =GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long id;
-    
-    @NotBlank(message = "name could not be empty")
-    private String Name;
-    
-    @Email(message = "Invalid Email Format")
+
+    @NotBlank(message = "Name could not be empty")
+    @Pattern(regexp = "^[A-Z][a-z]+\\s[A-Z][a-z]+$", message = "Name must be in format: Firstname Lastname")
+    @Column(name = "name")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    @Column(name = "email")
     private String email;
-    
-    @Size(min = 8 , message ="minimum 8 characters required")
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    @Column(name = "password")
     private String password;
-    
+
+    @Column(name = "is_verified")
     private boolean isVerified;
-    
-    @NotBlank(message = "otp cann't be empty")
-    @Size(min =6 ,max =6, message = "6 digits otp required")
+
+    @NotBlank(message = "OTP cannot be empty")
+    @Size(min = 6, max = 6, message = "OTP must be 6 digits")
+    @Pattern(regexp = "\\d{6}", message = "OTP must contain only digits")
+    @Column(name = "otp")
     private String otp;
-    
-    
-    
 }
