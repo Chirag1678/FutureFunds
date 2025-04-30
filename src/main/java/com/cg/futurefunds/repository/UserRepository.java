f0432df69edb69122b0bf9c505ceee672763aacf
Chirag <chiraggarg1708@gmail.com>
2025-04-30 09:28:13 +0530

    [Chirag] ADD: Implemented user verfication endpoint

--- src/main/java/com/cg/futurefunds/repository/UserRepository.java
+++ src/main/java/com/cg/futurefunds/repository/UserRepository.java
@@ -5,8 +5,6 @@ import com.cg.futurefunds.model.User;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
--- src/main/java/com/cg/futurefunds/service/AuthServiceImpl.java
+++ src/main/java/com/cg/futurefunds/service/AuthServiceImpl.java
@@ -1,7 +1,5 @@
 package com.cg.futurefunds.service;
 
-import java.util.StringTokenizer;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
