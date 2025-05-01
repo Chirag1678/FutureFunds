b5f7f312ab84f5221ca2864ba95fccc7b2ed5f32
Chirag <chirag1678.be21@chitkara.edu.in>
2025-05-01 09:33:47 +0530

    [Chirag] MOD: Investment DTO updated

--- src/main/java/com/cg/futurefunds/dto/InvestmentPlanDTO.java
+++ src/main/java/com/cg/futurefunds/dto/InvestmentPlanDTO.java
@@ -3,9 +3,14 @@ package com.cg.futurefunds.dto;
 import com.cg.futurefunds.model.InvestmentType;
 import jakarta.validation.constraints.*;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 @Data
+@RequiredArgsConstructor
 public class InvestmentPlanDTO {
+    @Email(message = "Invalid email format")
+    private String userEmail;
+
     @NotBlank(message = "Investment name is required")
     @Size(min = 4, max = 100, message = "Name must not exceed 100 characters")
     private String name;
